c1448beb96de82f555f7b54b9f1f71cf42c2425c
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2025-05-20 21:26:08 +0900

    api 取得の機能を切り分け

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { fetchMovies, fetchMovieCategory } from "./api/api";
 import { MovieType, MovieGenreType } from "./types/types";
 import Movie from "./components/movie";
 
@@ -9,30 +10,19 @@ function App() {
   const [moviesGenre, setMoviesGenre] = useState<MovieGenreType[]>([]);
   const [count, setCount] = useState<number>(1);
 
-  const fetchMovies = async () => {
-    try {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ja-JP&page=${count}`);
-      const data = await response.json();
-      const newMovies = [...movies, ...data.results];
-      setMovies(newMovies);
-      setCount(count+1)
-    } catch (error) {
-      console.error('movies',error);
-    }
-  };
-  const fetchMovieCategory = async () => {
-    try {
-      const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=ja-JP`);
-      const data = await response.json();
-      setMoviesGenre(data.genres);
-    } catch (error) {
-      console.error('movies genre',error);
-    }
-  };
+  const loadMoview = async () => {
+    const newMovies = await fetchMovies(key, count);
+    setMovies([...movies, ...newMovies]);
+    setCount(count+1)
+  }
+  const loadMoviesGenre = async () => {
+    const moviesGenre = await fetchMovieCategory(key);
+    setMoviesGenre(moviesGenre);
+  }
 
   useEffect(() => {
-    fetchMovies();
-    fetchMovieCategory();
+    loadMoview();
+    loadMoviesGenre();
   }, [key]);
 
   return (
@@ -48,7 +38,7 @@ function App() {
           </li>
         ))}
       </ul>
-      <button onClick={() => fetchMovies()} >
+      <button onClick={() => loadMoview()} >
         もっと見る
       </button>
     </div>
---
+++ src/api/api.ts
@@ -0,0 +1,22 @@
+
+export const fetchMovies = async ( key:string, count:number ) => {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ja-JP&page=${count}`);
+    const data = await response.json();
+    return data.results
+
+  } catch (error) {
+    console.error('movies',error);
+  }
+};
+
+export const fetchMovieCategory = async ( key:string ) => {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=ja-JP`);
+    const data = await response.json();
+    return data.genres
+
+  } catch (error) {
+    console.error('movies genre',error);
+  }
+};
