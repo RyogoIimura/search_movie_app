e4382d72621721706ea3e8b206ea506f508e2d0a
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2025-10-24 20:40:09 +0900

    parse エラー回避

--- src/hooks/fetchMoviesFunc.ts
+++ src/hooks/fetchMoviesFunc.ts
@@ -1,17 +1,14 @@
+
 export const getMovies = async (count: number) => {
   const res = await fetch(`/api/movies?count=${count}`);
-  if (!res.ok) {
-    throw new Error('Failed to fetch movies');
-  }
-  const movies = await res.json();
-  return movies;
+  if (!res.ok) throw new Error('Failed to fetch movies');
+
+  return res.text().then(t => (t ? JSON.parse(t) : []));
 };
 
 export const getMovieCategory = async () => {
   const res = await fetch(`/api/category`);
-  if (!res.ok) {
-    throw new Error('Failed to fetch category');
-  }
-  const category = await res.json();
-  return category;
+  if (!res.ok) throw new Error('Failed to fetch category');
+
+  return res.text().then(t => (t ? JSON.parse(t) : []));
 };
